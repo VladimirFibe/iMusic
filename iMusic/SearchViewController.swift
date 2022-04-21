8bfb8973681cd97c3c475c3035b781196668fa83
Vladimir Fibe <motiw@me.com>
2022-04-21 06:49:42 +0600

    add searchbar

--- iMusic/SearchViewController.swift
+++ iMusic/SearchViewController.swift
@@ -4,13 +4,58 @@
 //
 //  Created by MacService on 21.04.2022.
 //
-
+import SwiftUI
 import UIKit
 
+struct Track {
+  var name: String
+  var artist: String
+}
 class SearchViewController: UITableViewController {
-  
+  let cellId = "cellId"
+  let tracks = [
+  Track(name: "Bad Guy", artist: "Billie Eilish"),
+  Track(name: "Bury a friend", artist: "Billie Eilish")
+  ]
+  let searchController = UISearchController(searchResultsController: nil)
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     view.backgroundColor = .white
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    setupSearchBar()
+  }
+  
+  private func setupSearchBar() {
+    navigationItem.searchController = searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+    searchController.searchBar.delegate = self
+  }
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    60
+  }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    tracks.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+    let track = tracks[indexPath.row]
+    cell.textLabel?.text = track.name + "\n" + track.artist
+    cell.textLabel?.numberOfLines = 2
+    cell.imageView?.image = #imageLiteral(resourceName: "Album")
+    return cell
+  }
+}
+// MARK: - UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    print(searchText)
+  }
+}
+struct SearchViewController_Previews: PreviewProvider {
+  static var previews: some View {
+    SwiftUIMainTabBarController()
+      .edgesIgnoringSafeArea(.all)
   }
 }
